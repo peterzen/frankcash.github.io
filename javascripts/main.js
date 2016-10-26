94b46b6796b1051ae4a47a7f724af15d5cd52942
frankcash <cashc@acm.org>
2016-10-26 15:33:03 -0400

    Adds angular

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
-console.log('This would be the main JS file.');
+var app = angular.module("app", ['angular-loading-bar']);
+
+
+
+var app = angular.module("app", ['angular-loading-bar']);
+
+app.controller("AppTest", function($scope, $http){
+  /**
+  * global vars
+  **/
+  app=this;
+  $scope.pullRequests = "";
+
+
+  /**
+  * pull requests
+  **/
+  $http.get('https://raw.githubusercontent.com/frankcash/frankcash.github.io/master/static/pr.json').success(function(data) {
+      $scope.pullRequests = data;
+  });
+});
